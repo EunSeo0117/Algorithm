4fd8b79a1a74555f62c530fbac813e849a03107d
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-09 10:31:56 +0900

    [Passed] 배열의 합 계산하기 (95ms, 9MB)

---
+++ 코드트리/trail0/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int arr[] = {3,1,4,5,9};
+        System.out.println(arr[1]+arr[3]+arr[4]);
+    }
+}
