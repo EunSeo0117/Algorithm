38b37c1613155c062e3e5c5945b164e02009f0c7
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-09 13:21:04 +0900

    [Passed] 0 나올때까지 출력 (131ms, 11MB)

---
+++ 코드트리/trail0/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = -1;
+        while (n!= 0) {
+            n = sc.nextInt();
+            if (n==0) break;
+            System.out.println(n);
+        }
+    }
+}
