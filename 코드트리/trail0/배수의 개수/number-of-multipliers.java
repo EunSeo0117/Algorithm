c558e329476b37a2ad11dec4339ba86f73e54f06
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-09 11:50:50 +0900

    [Passed] 배수의 개수 (137ms, 10MB)

---
+++ 코드트리/trail0/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        for (int i=0; i<10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int cnt3=0;
+        int cnt5=0;
+        for (int i=0; i<10; i++) {
+            if (arr[i]%3==0) cnt3+=1;
+            if (arr[i]%5==0) cnt5+=1;
+        }
+        System.out.println(cnt3 + " " + cnt5);
+    }
+}
