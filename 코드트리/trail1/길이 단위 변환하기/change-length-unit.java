f1001854307d046a6dc999632e1ec1ab5a987903
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-16 19:15:46 +0900

    [Passed] 길이 단위 변환하기 (99ms, 9MB)

---
+++ 코드트리/trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double a= 30.48;
+        double b = 160934;
+
+        System.out.printf("9.2ft = %.1fcm", 9.2 * a);
+        System.out.printf("\n1.3mi = %.1fcm", b*1.3);
+    }
+}
