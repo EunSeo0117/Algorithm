5aa3483aaaf7ab9cf5a99fb5091fe7293699dcee
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-17 20:47:08 +0900

    [Passed] 합을 복사하기 (91ms, 9MB)

---
+++ 코드트리/trail1/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=1,b=2,c=3;
+        int tmp=a+b+c;
+        a=tmp;
+        b=tmp;
+        c=tmp;
+
+        System.out.printf("%d %d %d", a,b,c);
+    }
+}
