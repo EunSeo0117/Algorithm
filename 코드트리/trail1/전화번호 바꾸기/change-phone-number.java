36d086347999a19a7257df511192c2d95c91b442
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-22 10:36:23 +0900

    [Passed] 전화번호 바꾸기 (136ms, 10MB)

---
+++ 코드트리/trail1/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String[] t = a.split("-");
+        System.out.printf("%s-%s-%s", t[0], t[2], t[1]);
+    }
+}
