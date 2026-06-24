d261f0a9e1b929766a2013a8c59d8bcc09c698ab
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-24 14:14:07 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (133ms, 10MB)

---
+++ 코드트리/trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static int pibo(int n) {
+        if (n==1) return 1;
+        if (n==2) return 1;
+        return pibo(n-1) + pibo(n-2);
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(pibo(n));
+
+    }
+}
