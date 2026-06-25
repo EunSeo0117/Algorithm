2dc91ffa4d0da5ba7d44959d5107651b88d3c7f0
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-06-25 13:53:57 +0900

    [Passed] 연속되는 수 3 (158ms, 11MB)

---
+++ 코드트리/trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            if (i>=1 && (arr[i-1] * arr[i] > 0)) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+
+            ans = Math.max(ans, cnt);
+        }
+
+        System.out.println(ans);
+    }
+}
