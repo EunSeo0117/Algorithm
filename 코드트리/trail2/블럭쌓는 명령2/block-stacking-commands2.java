37dcfee997591d7742a3eac458510be1ea659591
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-07-20 16:28:53 +0900

    [Passed] 블럭쌓는 명령2 (134ms, 10MB)

---
+++ 코드트리/trail2/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        int[] arr = new int[n+1];
+
+        for (int i=0; i<k; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for (int j=a; j<=b; j++) {
+                arr[j]++;
+            }
+        }
+
+        int max=0;
+        for (int i=1; i<=n;i++) {
+            if (arr[i]>max) max = arr[i];
+        }
+
+        System.out.println(max);
+
+
+    }
+}
