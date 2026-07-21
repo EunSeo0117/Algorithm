4f8e948c0e7448b732d16b71b637d3c69e441c7b
EunSeo Han <153796118+EunSeo0117@users.noreply.github.com>
2026-07-21 20:38:03 +0900

    [Passed] T를 초과하는 연속 부분 수열 (140ms, 10MB)

---
+++ 코드트리/trail2/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int cnt = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > t) {
+                cnt++;
+            } else {
+                max = Math.max(max, cnt);
+                cnt = 0;
+            }
+        }
+
+        max = Math.max(max, cnt);
+
+        System.out.println(max);
+    }
+}
